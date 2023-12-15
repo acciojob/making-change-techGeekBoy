fc77a06151d8eb3d85e4877c4e90aa8e2c063803
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2023-12-15 22:14:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+  // Define the values of each coin
+  const quarterValue = 25;
+  const dimeValue = 10;
+  const nickelValue = 5;
+  const pennyValue = 1;
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+  // Initialize variables to count the number of each coin
+  let quarters = 0;
+  let dimes = 0;
+  let nickels = 0;
+  let pennies = 0;
+
+  // Calculate the number of each coin
+  while (amount > 0) {
+    if (amount >= quarterValue) {
+      quarters++;
+      amount -= quarterValue;
+    } else if (amount >= dimeValue) {
+      dimes++;
+      amount -= dimeValue;
+    } else if (amount >= nickelValue) {
+      nickels++;
+      amount -= nickelValue;
+    } else {
+      pennies++;
+      amount -= pennyValue;
+    }
+  }
+
+  // Create and return the result object
+  const result = { q: quarters, d: dimes, n: nickels, p: pennies };
+  return result;
+}
+
+// Examples
+console.log(makeChange(47)); // Output: { "q": 1, "d": 2, "n": 0, "p": 2 }
+console.log(makeChange(24)); // Output: { "q": 0, "d": 2, "n": 0, "p": 4 }
